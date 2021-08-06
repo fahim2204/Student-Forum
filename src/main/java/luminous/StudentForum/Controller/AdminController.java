c759f4441460e8585cd6758bab115fc471f4b644
fahim2204 <fahimfaisal1998@gmail.com>
2021-08-06 19:14:08 +0600

    Admin block post implemented

--- src/main/java/luminous/StudentForum/Controller/AdminController.java
+++ src/main/java/luminous/StudentForum/Controller/AdminController.java
@@ -18,8 +18,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import luminous.StudentForum.model.Category;
+import luminous.StudentForum.model.Post;
 import luminous.StudentForum.model.User;
 import luminous.StudentForum.repository.CategoryRepository;
+import luminous.StudentForum.repository.PostRepository;
 import luminous.StudentForum.repository.UserRepository;
 
 @Controller
@@ -29,17 +31,18 @@ public class AdminController {
     private CategoryRepository categoryRepository;
     @Autowired
     private UserRepository userRepository;
-
+    @Autowired
+    private PostRepository postRepo;
 
     @PreAuthorize("hasAnyRole('ADMIN')")
     @GetMapping("/admin")
-    public String AdminIndex(){
+    public String AdminIndex() {
         return "admin/index";
     }
 
     @GetMapping("/admin/category")
-    public ModelAndView Category(ModelAndView modelAndView){
-        List<Category> categoryList =  categoryRepository.findAll();
+    public ModelAndView Category(ModelAndView modelAndView) {
+        List<Category> categoryList = categoryRepository.findAll();
         modelAndView.addObject("categoryList", categoryList);
         modelAndView.addObject("category", new Category());
         modelAndView.setViewName("/admin/category");
@@ -47,83 +50,116 @@ public class AdminController {
     }
 
     @PostMapping("/admin/category")
-    public String CategoryCreate(@Valid Category category, BindingResult result, Model model){
-        if(categoryRepository.findByCname(category.getCname()) != null){
+    public String CategoryCreate(@Valid Category category, BindingResult result, Model model) {
+        if (categoryRepository.findByCname(category.getCname()) != null) {
             result.addError(new FieldError("category", "cname", "Category Already exists!!"));
         }
-        if(result.hasErrors()){
-            List<Category> categoryList =  categoryRepository.findAll();
+        if (result.hasErrors()) {
+            List<Category> categoryList = categoryRepository.findAll();
             model.addAttribute("categoryList", categoryList);
             return "/admin/category";
-        }else{
+        } else {
             categoryRepository.save(category);
             return "redirect:/admin/category";
         }
     }
+
     @GetMapping("/admin/category/{cname}/edit")
-    public ModelAndView CategoryEdit(@PathVariable("cname") String cname, ModelAndView modelAndView){
-        List<Category> categoryList =  categoryRepository.findAll();
+    public ModelAndView CategoryEdit(@PathVariable("cname") String cname, ModelAndView modelAndView) {
+        List<Category> categoryList = categoryRepository.findAll();
         modelAndView.addObject("categoryList", categoryList);
         modelAndView.addObject("cname", cname);
         modelAndView.setViewName("/admin/category-edit");
         return modelAndView;
     }
+
     @PostMapping("/admin/category/{cname}/edit")
-    public String CategoryUpdate(@PathVariable("cname") String cname, @RequestParam("newcname") String newcname){
+    public String CategoryUpdate(@PathVariable("cname") String cname, @RequestParam("newcname") String newcname) {
         Category category = categoryRepository.findByCname(cname);
         category.setCname(newcname);
         categoryRepository.save(category);
         return "redirect:/admin/category";
     }
+
     @GetMapping("/admin/category/{cname}/delete")
-    public String CategoryDelete(@PathVariable("cname") String cname){
+    public String CategoryDelete(@PathVariable("cname") String cname) {
         categoryRepository.delete(categoryRepository.findByCname(cname));
         return "redirect:/admin/category";
     }
 
-
-    //# -----------------For User Operation----------------
+    // # -----------------For User Operation----------------
 
     @GetMapping("/admin/user")
-    public ModelAndView User(ModelAndView modelAndView){
-        List<User> userList =  userRepository.findAll();
+    public ModelAndView User(ModelAndView modelAndView) {
+        List<User> userList = userRepository.findAll();
         modelAndView.addObject("userList", userList);
         modelAndView.setViewName("/admin/user");
         return modelAndView;
     }
 
     @PostMapping("/admin/user")
-    public String UserCreate(@Valid Category category, BindingResult result, Model model){
-        if(categoryRepository.findByCname(category.getCname()) != null){
+    public String UserCreate(@Valid Category category, BindingResult result, Model model) {
+        if (categoryRepository.findByCname(category.getCname()) != null) {
             result.addError(new FieldError("category", "cname", "Category Already exists!!"));
         }
-        if(result.hasErrors()){
-            List<Category> categoryList =  categoryRepository.findAll();
+        if (result.hasErrors()) {
+            List<Category> categoryList = categoryRepository.findAll();
             model.addAttribute("categoryList", categoryList);
             return "/admin/category";
-        }else{
+        } else {
             categoryRepository.save(category);
             return "redirect:/admin/category";
         }
     }
+
     @GetMapping("/admin/user/{username}/edit")
-    public ModelAndView UserEdit(@PathVariable("username") String username, ModelAndView modelAndView){
-        List<Category> categoryList =  categoryRepository.findAll();
+    public ModelAndView UserEdit(@PathVariable("username") String username, ModelAndView modelAndView) {
+        List<Category> categoryList = categoryRepository.findAll();
         modelAndView.addObject("categoryList", categoryList);
         modelAndView.addObject("cname", username);
         modelAndView.setViewName("/admin/category-edit");
         return modelAndView;
     }
+
     @PostMapping("/admin/user/{username}/edit")
-    public String UserUpdate(@PathVariable("username") String username, @RequestParam("newcname") String newcname){
+    public String UserUpdate(@PathVariable("username") String username, @RequestParam("newcname") String newcname) {
         Category category = categoryRepository.findByCname(username);
         category.setCname(newcname);
         categoryRepository.save(category);
         return "redirect:/admin/category";
     }
+
     @GetMapping("/admin/user/{username}/delete")
-    public String UserDelete(@PathVariable("username") String username){
+    public String UserDelete(@PathVariable("username") String username) {
         categoryRepository.delete(categoryRepository.findByCname(username));
         return "redirect:/admin/category";
     }
+
+    // # -----------------For POST Operation----------------
+
+    @GetMapping("/admin/post")
+    public ModelAndView Post(ModelAndView modelAndView) {
+        modelAndView.addObject("postList", postRepo.getAllPostDetailsNoLimit());
+        modelAndView.setViewName("/admin/post");
+        return modelAndView;
+    }
+
+    @GetMapping("/admin/posts/delete/{id}")
+    public String DeletePost(@PathVariable("id") int id) {
+        postRepo.delete(postRepo.findById(id));
+        return "redirect:/admin/post";
+    }
+
+    @GetMapping("/admin/posts/change-status/{id}")
+    public String ChangePostStatus(@PathVariable("id") int id) {
+        Post oldPost = postRepo.findById(id);
+        if (oldPost.getStatus() == 1) {
+            oldPost.setStatus(2);
+        } else {
+            oldPost.setStatus(1);
+        }
+        postRepo.save(oldPost);
+        return "redirect:/admin/post";
+    }
+
 }
--- src/main/java/luminous/StudentForum/repository/PostRepository.java
+++ src/main/java/luminous/StudentForum/repository/PostRepository.java
@@ -18,20 +18,25 @@ public interface PostRepository extends JpaRepository<Post, Integer> {
 
    Post findById(int id);
 
-   @Query(value="SELECT username, cname, pbody, title, created_at, (SELECT COUNT(id) FROM views WHERE fk_post=p.id) AS views, (SELECT COUNT(id) FROM comments WHERE fr_post=p.id) AS comments, p.id FROM posts as p JOIN categories as c ON p.fk_category_id=c.id JOIN users as u ON p.fk_user_id = u.id ORDER BY p.created_at DESC LIMIT 6", nativeQuery = true)
+   // @Query(value="SELECT username, cname, pbody, title, created_at, (SELECT COUNT(id) FROM views WHERE fk_post=p.id) AS views, (SELECT COUNT(id) FROM comments WHERE fr_post=p.id) AS comments, p.id FROM posts as p JOIN categories as c ON p.fk_category_id=c.id JOIN users as u ON p.fk_user_id = u.id ORDER BY p.created_at DESC LIMIT 6", nativeQuery = true)
+   // public Collection getAllPostDetails();
+   @Query(value="SELECT * FROM postdetails where status=1 LIMIT 6", nativeQuery = true)
    public Collection getAllPostDetails();
    
    // @Query(value="SELECT username, cname, pbody, title, created_at, (SELECT COUNT(id) FROM views WHERE fk_post=p.id) AS views, (SELECT COUNT(id) FROM comments WHERE fr_post=p.id) AS comments, p.id FROM posts as p JOIN categories as c ON p.fk_category_id=c.id JOIN users as u ON p.fk_user_id = u.id", nativeQuery = true)
-   @Query(value="SELECT * FROM postdetails", nativeQuery = true)
+   @Query(value="SELECT * FROM postdetails where status=1", nativeQuery = true)
    public Page<Collection> getAllPostDetailsPagination(Pageable pageable);
 
-   @Query(value="SELECT * FROM postdetails WHERE cname=?1", nativeQuery = true)
+   @Query(value="SELECT * FROM postdetails", nativeQuery = true)
+   public Collection getAllPostDetailsNoLimit();
+
+   @Query(value="SELECT * FROM postdetails WHERE cname=?1 AND status=1", nativeQuery = true)
    public Page<Collection> getCategoryPostDetailsPagination(String cat,Pageable pageable);
 
-   @Query(value="SELECT * FROM postdetails WHERE title LIKE %?1% ", nativeQuery = true)
+   @Query(value="SELECT * FROM postdetails WHERE title LIKE %?1% AND status=1", nativeQuery = true)
    public Page<Collection> getBySearchPostDetailsPagination(String shrc,Pageable pageable);
 
-   @Query(value="SELECT * FROM postdetails WHERE title LIKE '%?1%' ", nativeQuery = true)
+   @Query(value="SELECT * FROM postdetails WHERE title LIKE '%?1%' AND status=1", nativeQuery = true)
    public Collection getBySearchPostDetails(String shrc);
 
    @Query(value="SELECT * FROM postdetails WHERE username=?1", nativeQuery = true)
